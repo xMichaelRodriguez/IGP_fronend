096f3cebe7fdb566b0b1bc2b416618f6544a64d2
xMichaelRodriguez <scottlovos503@gmail.com>
2021-10-06 20:22:31 -0600

    narrador en lectura

--- src/components/UI/CardRead/CardReadScreen.js
+++ src/components/UI/CardRead/CardReadScreen.js
@@ -1,24 +1,27 @@
-import moment from 'moment'
-import 'moment/locale/es'
-
 import React, { useEffect, useState } from 'react'
-import {
-  useHistory,
-  useLocation,
-  useParams,
-} from 'react-router'
+import { useLocation, useParams } from 'react-router'
+
+import 'moment/locale/es'
+import moment from 'moment'
 import { fetchSync } from '../../../helpers/fetching'
 import { WaitScreen } from '../../wait/WaitScreen'
-
+import {
+  FaPauseCircle,
+  FaPlay,
+  FaTimesCircle,
+} from 'react-icons/fa'
+import { BiReset } from 'react-icons/bi'
+const speaker = new SpeechSynthesisUtterance()
+const synth = window.speechSynthesis
 export const CardReadScreen = () => {
   const { Id } = useParams()
   const location = useLocation().pathname.split('/')
-  const history = useHistory()
+
   let path = location[1].includes('noticias')
     ? 'noticies'
     : 'stories'
-
   const [dataToRead, setDataToRead] = useState({})
+
   useEffect(() => {
     ;(async function () {
       const data = await fetchSync(path + '/' + Id)
@@ -30,6 +33,24 @@ export const CardReadScreen = () => {
       }
     })()
   }, [Id, path])
+
+  const handleSpeak = (d) => {
+    console.log('hola')
+    speaker.voice = speechSynthesis.getVoices()[11]
+    speaker.text = d.title
+    speechSynthesis.speak(speaker)
+    speaker.text = d.body
+    speechSynthesis.speak(speaker)
+  }
+  const handlePause = () => {
+    synth.pause()
+  }
+  const handleResume = () => {
+    synth.resume()
+  }
+  const handleCancel = () => {
+    synth.cancel()
+  }
   return Object.entries(dataToRead).length === 0 ? (
     <div className='d-flex justify-content-center'>
       <WaitScreen />
@@ -51,12 +72,38 @@ export const CardReadScreen = () => {
           {dataToRead.body}
         </p>
 
-        <button
-          className='btn btn-link '
-          onClick={() => history.goBack()}
-        >
-          Volver
-        </button>
+        <div className='row mbb-3'>
+          <button
+            className='btn primary mx-1'
+            onClick={() => handleSpeak(dataToRead)}
+            title='Narrar'
+          >
+            <FaPlay />
+          </button>
+
+          <button
+            className='btn btn-outline-secondary mx-1'
+            onClick={handleResume}
+            title='Reanudar'
+          >
+            <BiReset />
+          </button>
+
+          <button
+            className='btn btn-outline-secondary mx-1'
+            onClick={handlePause}
+            title='pausar'
+          >
+            <FaPauseCircle />
+          </button>
+          <button
+            className='btn btn-outline-danger mx-1'
+            onClick={handleCancel}
+            title='Cancelar'
+          >
+            <FaTimesCircle />
+          </button>
+        </div>
       </div>
     </div>
   )
