c35bc9b057af3370c725311d44c64b43f97a9269
xMichaelRodriguez <scottlovos503@gmail.com>
2021-11-19 09:03:48 -0600

    push notifications

--- public/sw.js
+++ public/sw.js
@@ -11,6 +11,7 @@ self.addEventListener('push', (e) => {
   console.log('Notification Received');
   self.registration.showNotification(data.title, {
     body: data.message,
-    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1024px-Archlinux-icon-crystal-64.svg.png',
+    icon: './logo192.png',
+    vibrate: [200, 100, 200, 100, 200, 100, 400],
   });
 });
